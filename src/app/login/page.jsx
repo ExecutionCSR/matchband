7514755f22560dc9421aa6ab92b4cb0763e5f4d5
unknown <Caio_22364@ETEC.FURLAN>
2023-09-20 21:19:42 -0300

    arrumando cacagadas do caio

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -12,32 +12,32 @@ const Login = () => {
     return (
         <section className='grid bg-black grid-cols-2'>
             <div className='flex items-center justify-center'>
-                <div className=" absolute w-[500px] h-[500px] bg-white border-[3px] border-[#2400FF]" style={{ "clip-path": "polygon(100% 50%, 0 0, 0 100%)" }}>
+                <div className=" absolute w-[500px] h-[500px] bg-white border-[3px] border-[#2400FF]" style={{ "clipPath": "polygon(100% 50%, 0 0, 0 100%)" }}>
                 </div>
                 <Image className='z-10 w-[30rem] mr-[5rem]' src={Logo} />
             </div>
             <div className='bg-black flex flex-col'>
                 <div className='w-[60vh] h-fit cursor-pointer flex flex-col rounded-[25px] text-center border-black  bg-white justify - self-center justify-self-center border '>
                     <div className='p-14'>
-                        <h3 class='rounded-full   border-2 p-1 mt-3 border-black text-[#150190] text-[18px] font-bold'>Entre com google
+                        <h3 className='rounded-full   border-2 p-1 mt-3 border-black text-[#150190] text-[18px] font-bold'>Entre com google
                             <Image className='w-8 ml-5 inline-flex h-8' src={Google} />
                         </h3>
-                        <h3 class='rounded-full border-2 p-1 mt-3 border-black text-[#150190] text-[18] font-bold'>Entre com Facebook
+                        <h3 className='rounded-full border-2 p-1 mt-3 border-black text-[#150190] text-[18] font-bold'>Entre com Facebook
                             <Image className='w-8 ml-3 inline-flex h-8' src={Facebook} />
                         </h3>
-                        <h3 class='rounded-full t border-2 p-1 mt-3 border-black text-[#150190] text-[18] font-bold'>Entre com Instagram
+                        <h3 className='rounded-full t border-2 p-1 mt-3 border-black text-[#150190] text-[18] font-bold'>Entre com Instagram
                             <Image className='w-8 inline-flex ml-3 h-8' src={Instagram} />
                         </h3>
                     </div>
                     {/* formulario  */}
                     <form className=' w-[100%] cursor-pointer'>
-                        <label className='flex ml-5 text-black' for='email'>Email </label>
+                        <label className='flex ml-5 text-black' htmlFor='email'>Email </label>
                         <input name='email' type='email' class='w-[90%] border-2 border-black  rounded-full peer'></input>
                         <p className=" invisible peer-invalid:visible text-pink-600 text-sm">
                             Por favor insira um email valido.</p>
 
 
-                        <label className='flex ml-5 text-black' for='password'>Senha</label>
+                        <label className='flex ml-5 text-black' htmlFor='password'>Senha</label>
 
                         <input name='password' type='password' class='w-[90%] border-2 border-black  rounded-full peer '></input>
                         <p className=" invisible peer-invalid:visible text-pink-600 text-sm">
