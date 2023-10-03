ac860af9c9d60cf136c0aeb63370a799a61bd826
ExecutionCSR <silvakaio611@gmail.com>
2023-10-02 21:59:42 -0300

    faznedo o layout

--- src/app/cadastro/page.jsx
+++ src/app/cadastro/page.jsx
@@ -1,48 +1,15 @@
 import Button from "@/components/button";
 import Header from "@/components/header";
 import Input from "@/components/input";
+import Layout from "@/components/layout1";
 import Link from "next/link";
 
 const Cadastro = () => {
     return (
-        <>
-            <Header />
-            <div className='m-4 flex flex-col justify-center  items-center'>
-                <div className='h-[3rem] w-[90%] bg-[#150190] rounded border-[1px] border-[#2400FF] '></div>
-                <div className='z-10 h-[2rem] w-[50%] rounded border-[1px] bg-[#2107C5] border-[#2400FF] mt-[-1rem]'></div>
-            </div>
+        <Layout >
             <div className="  w-[90% ] max-w-[80%] h-[81vh] text-black">
                 <form className=" flex flex-col ml-[25%] p-5 h-[98%] border-solid border-4 border-[#2400FF]  bg-white rounded-2xl" action="">
                     <h1 className="text-indigo-800 text-[40px] text-center font-bold">Cadastre suas informaçoes  abaixo</h1>
-                    {/*                     <div className="p-2">
-                        <label className="  text-center text-black text-[30px] font-bold" htmlFor="nome">Nome:</label>
-                        <input className=" ml-7 w-[40%] rounded-xl border-black border-2" name="nome" type="text" />
-                    </div>
-                    <div className="p-2">
-                        <label className="text-center text-black text-[30px] font-bold" htmlFor="email">Email:</label>
-                        <input className="ml-7 w-[40%] rounded-xl border-black border-2" name="email" type="text" />
-                    </div>
-                    <div className="p-2" >
-                        <label className="text-center text-black text-[30px] font-bold" htmlFor="telefone">Celular:</label>
-                        <input className="ml-7 w-[20%] rounded-xl border-black border-2" name="telefone" type="text" />
-                    </div>
-                    <div className="p-2">
-                        <label className="text-center text-black text-[30px] font-bold" htmlFor="idade"> Data de nascimento:</label>
-                        <input className="ml-7 w-[11%] rounded-xl border-black border-2" name="idade" type="text" />
-                    </div>
-                    <div className="p-2">
-                        <label className="text-center text-black text-[30px] font-bold" htmlFor="senha">Senha:</label>
-                        <input className="ml-7 w-[30%] rounded-xl border-black border-2" name="senha" type="Password" />
-                    </div>
-                    <div className="p-2">
-                        <label className="text-center text-black text-[30px] font-bold" htmlFor="confirma">Confirme sua senha:</label>
-                        <input className="ml-7 w-[30%] rounded-xl border-black border-2" name="confirma" type="password" />
-                    </div>
-                    <div className="p-2">
-                        <label className="text-center text-black text-[30px] font-bold" htmlFor="endereco">Endereço:</label>
-                        <input className="ml-7 w-[75%] rounded-xl border-black border-2" type="text" name="endereco" />
-                    </div> */}
-
                     <Input nome="nome" titulo="Nome:" />
                     <Input tipo="email" nome="email" titulo="Email:" />
                     <Input nome="celular" titulo="Celular:" width="w-[20%]" />
@@ -62,12 +29,7 @@ const Cadastro = () => {
 
                 </form>
             </div>
-            <div className='m-4 flex flex-col-reverse justify-center  items-center'>
-                <div className='h-[3rem] w-[90%] bg-[#150190] rounded border-[1px]  border-[#2400FF]'></div>
-                <div className='z-10 h-[2rem] w-[50%] rounded border-[1px] bg-[#2107C5] border-[#2400FF] mb-[-1rem]'></div>
-            </div>
-
-        </>
+        </Layout>
     );
 };
 
--- src/app/paginainicial/page.jsx
+++ src/app/paginainicial/page.jsx
@@ -1,21 +1,25 @@
 
 import Header from "@/components/header";
+import Layout from "@/components/layout1";
 import Login from "../login/page";
 
 const PaginaInicial = () => {
     return (
-        <main className='bg-black'>
-            <Header />
-            <div className='m-4 flex flex-col justify-center  items-center'>
-                <div className='h-[3rem] w-[90%] bg-[#150190] rounded border-[1px] border-[#2400FF] '></div>
-                <div className='z-10 h-[2rem] w-[50%] rounded border-[1px] bg-[#2107C5] border-[#2400FF] mt-[-1rem]'></div>
-            </div>
+        <Layout>
             <Login />
-            <div className='m-4 flex flex-col-reverse justify-center  items-center'>
-                <div className='h-[3rem] w-[90%] bg-[#150190] rounded border-[1px] border-[#2400FF] '></div>
-                <div className='z-10 h-[2rem] w-[50%] rounded border-[1px] bg-[#2107C5] border-[#2400FF] mb-[-1rem]'></div>
-            </div>
-        </main>
+        </Layout>
+        /*         <main className='bg-black'>
+                    <Header />
+                    <div className='m-4 flex flex-col justify-center  items-center'>
+                        <div className='h-[3rem] w-[90%] bg-[#150190] rounded border-[1px] border-[#2400FF] '></div>
+                        <div className='z-10 h-[2rem] w-[50%] rounded border-[1px] bg-[#2107C5] border-[#2400FF] mt-[-1rem]'></div>
+                    </div>
+                    
+                    <div className='m-4 flex flex-col-reverse justify-center  items-center'>
+                        <div className='h-[3rem] w-[90%] bg-[#150190] rounded border-[1px] border-[#2400FF] '></div>
+                        <div className='z-10 h-[2rem] w-[50%] rounded border-[1px] bg-[#2107C5] border-[#2400FF] mb-[-1rem]'></div>
+                    </div>
+                </main> */
 
     )
 }
