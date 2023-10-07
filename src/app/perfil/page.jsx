ccf06a3b1eab841e946f95977fe5ad1436b53884
alfredsilvasilva <alfredmetal@gmail.com>
2023-10-06 21:25:54 -0300

    atualizaçao perfil

--- src/app/perfil/page.jsx
+++ src/app/perfil/page.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import foto from "../../../public/foto.jpeg";
+import Button from "@/components/button";
 import Input from "@/components/input";
+import Link from "next/link";
 const Criarperfil = () => {
   return (
     <>
@@ -11,39 +13,49 @@ const Criarperfil = () => {
 
       <div>
         <div className="flex  flex-wrap  w-[80% ] h-[33rem] m-20  border-solid border-2  border-black ">
-         <div className="w-[100%] text-center text-[35px] h-5  text-indigo-800  font-bold font-['Inter']">
-          <h1>Crie seu perfil</h1></div> 
-          
+          <div className="w-[100%] text-center text-[35px] h-5  text-indigo-800  font-bold font-['Inter']">
+            <h1>Crie seu perfil</h1></div>
+
           <div className="w-[30%] items-center  text-center h-[30%]  mr-5">
-          <p>Coloque sua foto de perfil</p>
-          <div className="ml-[20%]">
-          <Image
-          
-          src={foto}
-          width={200}
-          height={100}
-          alt="Picture of the Usuario"
-          />
+            <p>Coloque sua foto de perfil</p>
+            <div className="ml-[20%] cursor-pointer">
+              <Image
+                
+                src={foto}
+                width={200}
+                height={100}
+                alt="Picture of the Usuario"
+              />
+            </div>
+
           </div>
-       
-          </div>
-          <div className="w-[60%] h-[40%] border-2">
-            <input className="w-[100%] h-[90%]  rounded-[25px] border-2 border-black" type="text" />
+          <div className="w-[60%] mt-5 h-[35%] border-2">
+            <p>Escreva sua bio:</p>
+            <input className="w-[100%] h-[80%]  rounded-[25px] border-2 border-black" type="text" />
           </div>
 
-      
-            <div className="w-[30%] h-[40%] border-2 ml-5 text-center mr-3">
-              <p>Escreva as hashtags dos seus gêneros de música:</p>
-              <input className="w-[100%] h-[70%] flex max-h-44 border-2 mr-8 rounded-[25px]  border-black" type="text" />
+
+          <div className="w-[30%] mt-10 h-[35%] border-2 ml-5 text-center mr-3">
+            <p>Escreva as hashtags dos seus gêneros de música:</p>
+            <input className="w-[100%] h-[60%] flex max-h-44 border-2 mr-8 rounded-[25px]  border-black" type="text" />
+          </div>
+          <div className="w-[60%] text-center h-[40%] border-2">
+            <p>Poste algumas fotos e vídeos</p>
+            <div className="w-[90%] h-[78%] flex items-start border-2 border-black rounded-[25px] ml-5 ">
+              <button className="w-12 h-12  bg-red-50 border-2 border-black m-3">+</button>
             </div>
-            <div className="w-[60%] text-center h-[40%] border-2">
-              <p>Poste algumas fotos e vídeos</p>
-              <div className="w-[90%] h-[78%] justify-start border-2 border-black rounded-[25px] ml-5 ">
-                <button className="w-12 h-12 bg-red-50 border-2 border-black m-3">+</button>
-              </div>
-            </div>
-      
+          </div>
+          <div className=" w-full space-x-[50rem]  text-lg">
+            <Link href="/">
+              <Button class="border-2 ml-10 pl-8 pr-8 border-black rounded-2xl mr-11 text-black active:bg-[#150190] active:text-[white]" titulo="Voltar" />
+            </Link>
+            <Link href="/perfil">
+              <Button class="border-2    pl-8 pr-8 border-black rounded-2xl text-black bg-white active:bg-[#150190] active:text-[white]" titulo="Continuar" />
+            </Link>
+          </div>
         </div>
+
+
       </div>
 
       <div className="m-4 flex flex-col-reverse justify-center  items-center">
