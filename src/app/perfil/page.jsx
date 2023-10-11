36d7a5baa227190f667bfc26746bce1851cf80d0
Caio_SR.exe <89546145+ExecutionCSR@users.noreply.github.com>
2023-10-11 23:28:35 +0000

    ajuste cadastro perfil

--- src/app/perfil/page.jsx
+++ src/app/perfil/page.jsx
@@ -1,26 +1,22 @@
 import Image from "next/image";
 import foto from "../../../public/foto.jpeg";
 import Button from "@/components/button";
-import Input from "@/components/input";
 import Link from "next/link";
+import Layout from "@/components/layout1";
+import { FaPlus } from "react-icons/fa6";
 const Criarperfil = () => {
   return (
-    <>
-      <div className="m-4 flex flex-col justify-around items-center">
-        <div className="h-[3rem] w-[90%] bg-[#150190] rounded border-[1px] border-[#2400FF] "></div>
-        <div className="z-10 h-[2rem] w-[50%] rounded border-[1px] bg-[#2107C5] border-[#2400FF] mt-[-1rem]"></div>
-      </div>
-
-      <div>
-        <div className="flex  flex-wrap  w-[80% ] h-[33rem] m-20  border-solid border-2  border-black ">
-          <div className="w-[100%] text-center text-[35px] h-5  text-indigo-800  font-bold font-['Inter']">
-            <h1>Crie seu perfil</h1></div>
+    <Layout >
+      <div className="text-black">
+        <div className="flex  flex-wrap  w-[80% ] h-[33rem] m-20  border-solid border-4  border-[#2400FF] bg-white rounded-lg">
+          <div className="w-[100%] text-center text-[35px] h-5  text-indigo-800  font-bold">
+            <h1>Crie seu perfil</h1>
+          </div>
 
           <div className="w-[30%] items-center  text-center h-[30%]  mr-5">
             <p>Coloque sua foto de perfil</p>
             <div className="ml-[20%] cursor-pointer">
               <Image
-                
                 src={foto}
                 width={200}
                 height={100}
@@ -29,40 +25,36 @@ const Criarperfil = () => {
             </div>
 
           </div>
-          <div className="w-[60%] mt-5 h-[35%] border-2">
+          <div className="w-[60%] mt-5 h-[35%] ">
             <p>Escreva sua bio:</p>
             <input className="w-[100%] h-[80%]  rounded-[25px] border-2 border-black" type="text" />
           </div>
 
-
-          <div className="w-[30%] mt-10 h-[35%] border-2 ml-5 text-center mr-3">
+          <div className="w-[30%] mt-10 h-[35%] ml-5 text-center mr-3">
             <p>Escreva as hashtags dos seus gêneros de música:</p>
             <input className="w-[100%] h-[60%] flex max-h-44 border-2 mr-8 rounded-[25px]  border-black" type="text" />
           </div>
-          <div className="w-[60%] text-center h-[40%] border-2">
+          <div className="w-[60%] text-center h-[40%] ">
             <p>Poste algumas fotos e vídeos</p>
             <div className="w-[90%] h-[78%] flex items-start border-2 border-black rounded-[25px] ml-5 ">
+              <div className="w-12 h-12  bg-white border-2 border-black m-3 text-black flex justify-center items-center rounded-[1rem]">
+                <FaPlus size={30} />
+              </div>
+
               <button className="w-12 h-12  bg-red-50 border-2 border-black m-3">+</button>
             </div>
           </div>
-          <div className=" w-full space-x-[50rem]  text-lg">
+          <div className=" w-full  text-lg flex items-center justify-between p-4">
             <Link href="/">
-              <Button class="border-2 ml-10 pl-8 pr-8 border-black rounded-2xl mr-11 text-black active:bg-[#150190] active:text-[white]" titulo="Voltar" />
+              <Button class="border-2 pl-8 pr-8 border-black rounded-2xl text-black active:bg-[#150190] active:text-[white]" titulo="Voltar" />
             </Link>
             <Link href="/perfil">
-              <Button class="border-2    pl-8 pr-8 border-black rounded-2xl text-black bg-white active:bg-[#150190] active:text-[white]" titulo="Continuar" />
+              <Button class="border-2 pl-8 pr-8 border-black rounded-2xl text-black bg-white active:bg-[#150190] active:text-[white]" titulo="Continuar" />
             </Link>
           </div>
         </div>
-
-
       </div>
-
-      <div className="m-4 flex flex-col-reverse justify-center  items-center">
-        <div className="h-[3rem] w-[90%] rounded-[25%] bg-[#150190] border-[1px]  border-[#2400FF] bg-"></div>
-        <div className="z-10 h-[2rem] w-[50%] rounded border-[1px] bg-[#2107C5] border-[#2400FF] mb-[-1rem]"></div>
-      </div>
-    </>
+    </Layout>
   );
 };
 
